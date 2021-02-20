b2008351eb2171952329a65a451e316368613932
Sahha2001000 <46458055+Sahha2001000@users.noreply.github.com>
2021-02-20 22:07:44 +0200

    Create full functional, does not have exe file

---
+++ Convector.py
@@ -0,0 +1,48 @@
+# libary for getting info from internet
+import requests
+# libary for parse html page,and work him
+from bs4 import BeautifulSoup
+
+
+class Convector(object):
+    # create for describe user(bot-test)
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"}
+
+    # Constructor
+    def __init__(self, currencyFromExchange='USD', currencyToExchange='UAH'):
+        self.currencyFromExchange = currencyFromExchange
+        self.currencyToExchange = currencyToExchange
+
+    def getURL(self):
+        return f'https://www.google.com/search?q={self.currencyFromExchange}+TO+{self.currencyToExchange}'
+
+    def getCurrencyPrice(self):
+        try:
+            # Create url for find info in Google <fromCurr> To <toCurr>
+            linkInfoCurrency = f'https://www.google.com/search?q={self.currencyFromExchange}+TO+{self.currencyToExchange}'
+            # GET HTML
+            fullPage = requests.get(linkInfoCurrency, headers=self.headers)
+            # PARSING
+            parsePage = BeautifulSoup(fullPage.content, 'html.parser')
+            # Find information
+            findInfoCurrency = parsePage.findAll('span', {'class': 'DFlfde', 'class': 'SwHCTb'})
+            # return <float> cost { <fromCurr> To <toCurr> }
+            print(findInfoCurrency[0].text.split(','))
+            print(findInfoCurrency)
+            print(linkInfoCurrency )
+            return float('.'.join((findInfoCurrency[0].text.split(','))))
+        except ValueError:
+            self.getCurrencyPrice()
+        except IndexError:
+            self.getCurrencyPrice()
+        except TypeError:
+            self.getCurrencyPrice()
+
+    def convector(self,amount = 100):
+        try:
+            return  self.getCurrencyPrice() * amount
+        except TypeError:
+            self.convector()
+
+
